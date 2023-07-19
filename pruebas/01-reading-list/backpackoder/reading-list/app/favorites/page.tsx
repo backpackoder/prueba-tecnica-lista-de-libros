23fbbc0e8b81e3db01477125e3e093db9fd96f37
harderth <th.barbiera@outlook.fr>
2023-07-19 00:29:05 -0600

    Comment Exported Component

--- pruebas/01-reading-list/backpackoder/reading-list/app/favorites/page.tsx
+++ pruebas/01-reading-list/backpackoder/reading-list/app/favorites/page.tsx
@@ -6,8 +6,8 @@ export default async function Favorites() {
   return (
     <article className="flex flex-col items-center justify-center gap-4">
       <h2 className="text-4xl">
-        Mis libros favoritos (<FavListLength />
-        ):
+        Mis libros favoritos
+        {/* (<FavListLength />): */}
       </h2>
 
       <BooksList />
